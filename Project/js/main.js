17b361327c1c2b7c065d46c6919bc1cfe20b1f78
Xedll <97981835+Xedll@users.noreply.github.com>
2022-11-26 22:03:47 +0300

    Little changes
    A bit redesigned language choise and ui

--- Project/js/main.js
+++ Project/js/main.js
@@ -3,27 +3,55 @@
 
 const searchBtn = document.querySelector('.main__button');
 const searchCity = document.querySelector('.main__input');
-const searchLang = document.querySelector('.main__lang');
+const searchLang = document.querySelector('.lang__select');
 let temp = [];
 const APIKey = '3ffe2ea29639380a36dd285c213cf530';
-
-window.onload = () => {
-   getInfo('Kazan', searchLang.value)
-   document.querySelector('.main__button').textContent = (document.querySelector('.main__lang').value == 'ru' ? 'Поиск' : 'Search')
-   document.querySelector('.main__input').placeholder = (document.querySelector('.main__lang').value == 'ru' ? 'Город' : 'City')
+let langs = {
+   'en': {
+      'temperature': 'Temperature',
+      'wind': 'Wind',
+      'speed': 'mps',
+      'search': 'Search',
+      'history': 'Search history',
+      'city': 'City',
+      'language': 'Language',
+   },
+   'ru': {
+      'temperature': 'Температура',
+      'wind': 'Ветер',
+      'speed': 'м/с',
+      'search': 'Поиск',
+      'history': 'История поиска',
+      'city': 'Город',
+      'language': 'Язык',
+   }
 }
 
-const editInfo = (info) => {
+window.onload = () => {
+   getInfo('Казань', 'ru')
+   setLang()
+}
+
+const editInfo = function (info, language) {
    document.querySelector('.main__city').textContent = info.city;
    document.querySelector('.main__country').textContent = info.country;
-   document.querySelector('.temp__now').textContent = "Температура: " + info.temp + "°C";
+   document.querySelector('.temp__now').textContent = langs[language].temperature + ': ' + info.temp + "°C";
    document.querySelector('.weather__main').textContent = info.weather.main;
    document.querySelector('.weather__description').textContent = info.weather.description[0].toUpperCase() + info.weather.description.slice(1);
    document.querySelector('.weather__logo').src = `http://openweathermap.org/img/wn/${info.weather.icon}@2x.png`;
-   document.querySelector('.main__wind').textContent = "Ветер: " + info.wind + 'м/c';
+   document.querySelector('.main__wind').textContent = langs[language].wind + ": " + info.wind + ' ' + langs[language].speed;
 }
 
-const getInfo = (city, lang) => {
+const setLang = function () {
+   searchBtn.textContent = langs[searchLang.value].search
+   searchCity.placeholder = langs[searchLang.value].city
+   document.querySelector('.log__title').textContent = langs[searchLang.value].history
+   document.querySelector('.lang__title').textContent = langs[searchLang.value].language
+   editInfo(temp[document.querySelector('.log__logs').value], searchLang.value)
+
+}
+
+const getInfo = function (city, lang) {
    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric&lang=${lang}`)
       .then((data) => {
          return data.json()
@@ -61,7 +89,7 @@ const getInfo = (city, lang) => {
          return info
       })
       .then((res) => {
-         editInfo(res[document.querySelector('.log__logs').value])
+         editInfo(res[document.querySelector('.log__logs').value], lang)
       })
       .catch((err) => {
          console.log(err)
@@ -69,12 +97,11 @@ const getInfo = (city, lang) => {
 }
 
 document.querySelector('.log__logs').onchange = function (e) {
-   editInfo(temp[document.querySelector('.log__logs').value])
+   editInfo(temp[document.querySelector('.log__logs').value], searchLang.value)
 }
 
-document.querySelector('.main__lang').onchange = function () {
-   document.querySelector('.main__button').textContent = (document.querySelector('.main__lang').value == 'ru' ? 'Поиск' : 'Search')
-   document.querySelector('.main__input').placeholder = (document.querySelector('.main__lang').value == 'ru' ? 'Город' : 'City')
+searchLang.onchange = function () {
+   setLang()
 }
 
 searchBtn.onclick = function (e) {
